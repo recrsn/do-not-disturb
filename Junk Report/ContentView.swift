459578e65016c291be45ed3db5dd8c7197dca0b1
Amitosh Swain Mahapatra <amitosh@recrsn.com>
2024-05-25 11:30:43 +0530

    Initial Commit

---
+++ Junk Report/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Junk Report
+//
+//  Created by Amitosh Swain Mahapatra on 25/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Junk Report/Junk_ReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Junk_ReportApp.swift
+//  Junk Report
+//
+//  Created by Amitosh Swain Mahapatra on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Junk_ReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
